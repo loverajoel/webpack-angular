e3e1494e913871240be15298a4bba936f6915d64
Joel <joelalovera@gmail.com>
2015-02-14 18:21:01 -0300

    fix workflow

--- app/entry.js
+++ app/entry.js
@@ -1,1 +1,2 @@
+require('angular');
 require("./hello/hello.js");
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,34 @@ var path = require("path");
 var webpack = require('("../../../../node_modules/webpack');
 var CommonsChunkPlugin = require("../../../../node_modules/webpack/lib/optimize/CommonsChunkPlugin");
 
-module.exports = {
+var bower_dir = __dirname + '/bower_components';
+
+var config = {
+    addVendor: function (name, path) {
+        this.resolve.alias[name] = path;
+        this.module.noParse.push(new RegExp(path));
+    },
     entry: {
         app: "./app/entry.js",
-        vendor: ["./app/commons/angular", "./app/commons/jquery"]
+        vendor: ["angular"]
+    },
+    resolve: {
+        alias: {}
     },
     output: {
         path: "./build",
-        // publicPath: '/app/',
+        publicPath: '/public/',
         filename: "[name].bundle.js",
-        // chunkFilename: "[id].chunk.js"
+        chunkFilename: "[id].chunk.js"
     },
-    plugins: [
-        new CommonsChunkPlugin("vendor", "vendor.bundle.js")
-    ]
+    module: {
+        noParse : []
+    },
+
+    plugins: [ new CommonsChunkPlugin("vendor", "vendor.bundle.js") ]
 };
+
+config.addVendor('angular', bower_dir + '/angular/angular.js');
+
+
+module.exports = config;
